9ee81322ad2b04096754579fb32a7b815671bbe0
Clementmartin2002 <cl.martin@student.isartdigital.com>
2024-04-06 12:26:47 +0200

    Add vector graphic ans wip situation 3

--- Assets/Scripts/Situation3/ManagerSituation3.cs
+++ Assets/Scripts/Situation3/ManagerSituation3.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class ManagerSituation3 : MonoBehaviour
 {
 	[SerializeField] private List<Book> allBook;
+	[SerializeField] private GameObject bookContainer;
+	[SerializeField] private GameObject fullscreenBook;
 
 	private void Start()
 	{
 		foreach (var book in allBook)
 		{
-			book.onClickedBook += Book_onClickedBook; ;
+			book.onClickedBook += Book_onClickedBook;
 		}
 	}
 
 	private void Book_onClickedBook(Book sender)
 	{
-		;
+		bookContainer.SetActive(false);
+		fullscreenBook.SetActive(true);
 	}
 }
