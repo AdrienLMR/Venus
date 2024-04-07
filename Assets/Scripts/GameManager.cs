be6df0bda457e0087ebf7d0c6f16ce992a509302
Andy <andymessorem@gmail.com>
2024-04-07 15:29:33 +0200

    Added more sounds and music

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -14,8 +15,11 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject mapScreen = default;
     [SerializeField] private GameObject stepOneScreen = default;
 
-    [Header("Sounds")]
+    [Header("Musics")]
     [SerializeField] private List<AudioSource> Themes;
+
+    [Header("Sounds")]
+    [SerializeField] private List<AudioSource> sfx;
 
     // Update is called once per frame
     void Update()
@@ -23,27 +27,54 @@ public class GameManager : MonoBehaviour
         if (!main && (titleScreen.activeSelf || mapScreen.activeSelf))
         {
             main = true;
-            playOnly(0);
+            playOnlyMusic(0);
             stepOne = false;
         }
         else if (!stepOne && stepOneScreen.activeSelf)
         {
             stepOne = true;
-            playOnly(1);
+            playOnlyMusic(1);
             main = false;
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            sfx[0].Play();
         }
     }
 
-    void playOnly(int music)
+    void playOnlyMusic(int music)
     {
         // 0 : Menu & Map
         // 1 : Dialog
+        // 2 : Leopold
+        // 3 : Esther
+        // 4 : Louise
+        // 5 : Tobias
         for (int i = 0; i < Themes.Count; i++)
         {
             if(i == music)
                 Themes[i].Play();
             else
                 Themes[i].Stop();
+        }
+    }
+
+    void playOnlySfx(int effect)
+    {
+        // 1 : Victory
+        // 2 : Game Over
+        // 3 : Bell
+        // 4 : Angry Kid
+        // 5 : Wood Crack
+        // 6 : Book
+        // 7 : Cough
+        for (int i = 1; i < sfx.Count; i++)
+        {
+            if (i == effect)
+                sfx[i].Play();
+            else
+                sfx[i].Stop();
         }
     }
 }
